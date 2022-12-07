5510eb8c76f539ebb5316a5fbb2a2e5093cef56a
MalikiHacker <malikihacker@gmail.com>
2022-12-07 17:38:11 +0000

    module complex form

---
+++ src/app/complex-form/complex-form.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {ComplexFormRoutingModule} from './complex-form-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComplexFormRoutingModule
+  ]
+})
+export class ComplexFormModule {
+}
